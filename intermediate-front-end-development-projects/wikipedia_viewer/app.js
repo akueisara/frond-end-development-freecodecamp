e6feeaefb0651ab0f3dc261b038b24585306136e
Kuei <akueisara39@gmail.com>
2016-09-10 21:07:39 -0700

    Add wikipedia viewer

--- intermediate-front-end-development-projects/wikipedia_viewer/app.js
+++ intermediate-front-end-development-projects/wikipedia_viewer/app.js
@@ -1,43 +1,23 @@
 $("#submit").on("click", function() {
     $("#output").empty();
     var searchTerm = $("#searchTerm").val();
-    // console.log(searchTerm);
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm;
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
     $.ajax({
         url: url,
-        method: "GET",
-        dataType: "json",
+        type: "GET",
+        async: false,
+        dataType: "jsonp",
         success: function(data) {
-            console.log(data);
+            // console.log(data);
             for (var i = 0; i < data[1].length; i++) {
-                $("#output").prepend("<div><div class='well'><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+                $("#output").prepend("<div class='resultlink'><a href=" + data[3][i] + " target='_blank'><div class='well'><h2>" + data[1][i] + "</h2>" + "<p>" + data[2][i] + "</p></div></a></div>");
+
             }
             if (data[1].length == 0) {
                 $("#output").prepend("<div class='result'><h5>Sorry, no results were found.</h5></div>");
             }
         }
+
     });
-});
 
-$("#searchTerm").keypress(function(e) {
-    if (e.keyCode === 13) {
-        var searchTerm = $("#searchTerm").val();
-        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
-        $.ajax({
-            url: url,
-            type: "GET",
-            async: false,
-            dataType: "json",
-            success: function(data, status, jqXHR) {
-                console.log(data);
-                for (var i = 0; i < data[1].length; i++) {
-                    $("#output").prepend("<div><div class='well'><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-                }
-                if (data[1].length == 0) {
-                    $("#output").prepend("<div class='result'><h5>Sorry, no results were found.</h5></div>");
-                }
-            }
-
-        });
-    }
 });
